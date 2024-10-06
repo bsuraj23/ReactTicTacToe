86761d27780a9cfd9ae493939a21c749b0284a71
unknown <bsuraj23@gmail.com>
2024-10-06 19:37:51 +0200

    adding new component

---
+++ src/components/Timer.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+function Timer({ type, degrees }) {
+  return (
+    <div
+      className={`hand ${type}`}
+      style={{ transform: `rotate(${degrees}deg)` }}
+    ></div>
+  );
+}
+//Change this when seconds are not in progress 
+//add mroe of the base code before handling the actual clock
+ClockHand.propTypes = {
+  type: PropTypes.string.isRequired,
+  degrees: PropTypes.number.isRequired,
+};
+
+export default ClockHand;
