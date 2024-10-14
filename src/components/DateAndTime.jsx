761e7fd9fcd2e431037fd52b4bce3d2ad0b9fb23
unknown <bsuraj23@gmail.com>
2024-10-14 08:06:34 +0200

    adding ewn depencies for framework

--- src/components/DateAndTime.jsx
+++ src/components/DateAndTime.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 
 function DateAndTime({ type, degrees }) {
   //adding new style as this is new component 
+  //adding new css framw work for the react project 
   return (
     <div
       className={`hand ${type}`}
